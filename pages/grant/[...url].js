75c3a89aa980c9f1d6dc625683eaf1a3220f69e7
Alex Grintsvayg <grin@lbry.com>
2021-03-17 13:04:37 -0400

    add claim content to grant page

--- pages/grant/[...url].js
+++ pages/grant/[...url].js
@@ -2,6 +2,8 @@ import { grants, sdk, fixUrl } from "../../sdk";
 import Head from "next/head";
 import Link from "next/link";
 
+const fetch = require('sync-fetch');
+
 export async function getStaticPaths() {
   const grantResults = grants()
 
@@ -25,7 +27,19 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Grant({grant}) {
-  // console.log(grant)
+  let content;
+  if (grant.value.source.media_type == "text/markdown") {
+    const md = fetch(
+      "https://cdn.lbryplayer.xyz/api/v4/streams/free/" +
+      [grant.name, grant.claim_id, grant.value.source.sd_hash.substring(0,6)].join("/")
+    ).text()
+    content = <pre>{md}</pre>
+  } else {
+    content = <iframe id="lbry-iframe" width="560" height="315"
+            src={"https://lbry.tv/$/embed/" + grant.name + "/" + grant.claim_id}
+            allowFullScreen></iframe>
+  }
+
   return (
     <div>
       <Head>
@@ -33,11 +47,21 @@ export default function Grant({grant}) {
       </Head>
 
       <main className="container max-w-2xl mx-auto">
-        <h1>{grant.value.title}</h1>
-        <img className="max-w-md" src={grant.value.thumbnail.url ?? "https://via.placeholder.com/300"}></img>
-        <Link href={"https://open.lbry.com/" + fixUrl(grant.short_url)}>
-          <a className="text-red-600">See this grant on LBRY</a>
+        <Link href="/">
+          <a className="text-blue-500">{"<"}- Back</a>
         </Link>
+
+        <h1>{grant.value.title}</h1>
+        <img className="max-w-md mb-4" src={grant.value.thumbnail.url ?? "https://via.placeholder.com/300"}></img>
+
+        {content}
+
+        <Link href={"https://open.lbry.com/" + fixUrl(grant.short_url)}>
+          <a className="text-red-600">Support this grant on LBRY</a>
+        </Link>
+
+        <pre>{JSON.stringify(grant.value, null, 2)}</pre>
+
       </main>
     </div>
   )
